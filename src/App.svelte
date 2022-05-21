8a152d7ca75fd1b312081a7f5e48b777ec361fc7
Kevin van den Hoek <kevinvandenhoek@gmail.com>
2022-05-21 16:52:02 +0200

    dark and light mode support

--- src/App.svelte
+++ src/App.svelte
@@ -1,30 +1,17 @@
 <script lang="ts">
-	export let name: string;
+  let name: string = "rat kakes";
 </script>
 
 <main>
-	<h1>Hello {name}!</h1>
-	<p>Visit the <a href="https://svelte.dev/tutorial">Svelte tutorial</a> to learn how to build Svelte apps.</p>
+  <h1>Hoi!</h1>
+  <p>Ik ben {name}</p>
 </main>
 
 <style>
-	main {
-		text-align: center;
-		padding: 1em;
-		max-width: 240px;
-		margin: 0 auto;
-	}
-
-	h1 {
-		color: #ff3e00;
-		text-transform: uppercase;
-		font-size: 4em;
-		font-weight: 100;
-	}
-
-	@media (min-width: 640px) {
-		main {
-			max-width: none;
-		}
-	}
+  main {
+    display: flex;
+    flex-direction: column;
+    text-align: center;
+    padding: 1em;
+  }
 </style>
