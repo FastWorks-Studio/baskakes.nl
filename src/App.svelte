be72c1591c89bf991f47eb86005de3ac9368b191
Kevin van den Hoek <kevinvandenhoek@gmail.com>
2022-05-22 12:43:03 +0200

    add video

--- src/App.svelte
+++ src/App.svelte
@@ -4,10 +4,17 @@
 
 <main>
   <Logo subtitle="work in progress" />
+  <video
+    preload="none"
+    src="./assets/video/showreel_liggend.mp4"
+    autoplay
+    muted
+  />
 </main>
 
 <style>
   main {
+    position: relative;
     display: flex;
     flex-direction: column;
     text-align: center;
@@ -16,5 +23,16 @@
     width: 100vw;
     height: 100vh;
     justify-content: center;
+  }
+
+  video {
+    position: absolute;
+    inset: 0;
+    width: 100vw;
+    height: 100vh;
+    object-fit: cover;
+    z-index: -10;
+    background-color: orange;
+    opacity: 0.2;
   }
 </style>
